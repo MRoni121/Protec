3d0ecda7c2aafecc26de7ffdbf0d1a676301a496
Mateus Roni <mateus-roni@hotmail.com>
2023-12-10 01:37:09 -0300

    :tada: add: plots

--- ied.m
+++ ied.m
@@ -12,6 +12,8 @@ fa            = 1920;               % Frequência de amostragem dos casos de sim
 num_ciclo     = fa/f;               % Número de amostras por ciclo (valor que pode ser fracionário)
 Ts            = 1/fa;               % Período de amostragem = Passo de integração do Simulink
 
+tamanho_janela_de_trip = 16;        % São necessários tamanho_janela_de_trip / 2 amostras para o trip
+
 alfa = cos(120*pi()/180) + 1i*sin(120*pi()/180);
 T = [1 1 1; 1 alfa*alfa alfa; 1 alfa alfa*alfa];
 
@@ -23,6 +25,7 @@ k = 0.5;
 % 3. Dados da simulação
 % -------------------------------------------------------------------------
 
+arquivo = 'ABC002_RED';
 ABC002_RED;
 
 % Criando os vetores de tempo e corrente
@@ -35,8 +38,8 @@ Ia_remoto = matriz(1:end, 5);
 Ib_remoto = matriz(1:end, 6);
 Ic_remoto = matriz(1:end, 7);
 
-for k = 1:length(tempo)
-  tempo(k) = (k-1)*Ts;
+for i = 1:length(tempo)
+  tempo(i) = (i-1)*Ts;
 end
 
 
@@ -61,28 +64,6 @@ Ic_local_f = Filtro_Analogico(1, Ic_local, tempo, 2*pi*fp, 2*pi*fs, Amin, Amax);
 Ia_remoto_f = Filtro_Analogico(1, Ia_remoto, tempo, 2*pi*fp, 2*pi*fs, Amin, Amax);
 Ib_remoto_f = Filtro_Analogico(1, Ib_remoto, tempo, 2*pi*fp, 2*pi*fs, Amin, Amax);
 Ic_remoto_f = Filtro_Analogico(1, Ic_remoto, tempo, 2*pi*fp, 2*pi*fs, Amin, Amax);
-
-
-% figure(1)
-% hold on;
-% zoom on;
-% grid on;
-% plot(tempo,Ia_1_10);
-% plot(tempo,Ib_1_10);
-% plot(tempo,Ic_1_10);
-% legend('iA','iB', 'iC');
-% title(["Correntes de linha das três fases na barra 10"]);
-
-
-% figure(2)
-% hold on;
-% zoom on;
-% grid on;
-% plot(tempo,Ia_1_10);
-% plot(tempo,Ia_1_10_f);
-% legend('Normal','Filtrado');
-% title(["Corrente de linha da fase A na barra 10 após filtragem analógica"]);
-
 
 
 %% ------------------------------------------------------------------------
@@ -114,6 +95,11 @@ izero_resistencia = zeros(1, length(tempo));       % Vetor que armazena a refer�
 ipos_resistencia = zeros(1, length(tempo));       % Vetor que armazena a referência o sinal de corrente de resistencia da sequência positiva
 ineg_resistencia = zeros(1, length(tempo));       % Vetor que armazena a referência o sinal de corrente de resistencia da sequência negativa
 
+sinal_trip_zero = zeros(1, length(tempo));       % Vetor que armazena a referência o sinal de trip da sequência zero
+sinal_trip_pos = zeros(1, length(tempo));       % Vetor que armazena a referência o sinal de trip da sequência positiva
+sinal_trip_neg = zeros(1, length(tempo));       % Vetor que armazena a referência o sinal de trip da sequência negativa
+
+
 % 5.1) Implementação do buffer circular
 
 tam_buffer  = 64;                          % Tamanho do buffer, em numero de amostras
@@ -136,6 +122,8 @@ instante_do_trip_neg = 0;
 deveria_atualizar_zero = 1;
 deveria_atualizar_pos = 1;
 deveria_atualizar_neg = 1;
+
+
 
 
 % % 5.2) Leitura dos ADs
@@ -190,19 +178,43 @@ while aux<length(tempo)
     ineg_operacao(aux) = abs(ineg_local(aux) - ineg_remoto(aux));
     ineg_resistencia(aux) = (abs(ineg_local(aux)) + abs(ineg_remoto(aux)))/2;
 
-    if(izero_operacao(aux) > IopMin && izero_operacao(aux) > k*izero_resistencia(aux) && deveria_atualizar_zero)
-        instante_do_trip_zero = tempo(aux);
-        deveria_atualizar_zero = 0;
+    if(izero_operacao(aux) > IopMin && izero_operacao(aux) > k*izero_resistencia(aux))
+        sinal_trip_zero(aux) = 1;
+        
+        % Janela deslizante
+        if(aux > tamanho_janela_de_trip && deveria_atualizar_zero)
+            media_zero = mean(sinal_trip_zero(aux - tamanho_janela_de_trip + 1:aux));
+            if(media_zero > 0.5)
+                instante_do_trip_zero = tempo(aux);
+                deveria_atualizar_zero = 0;
+            end
+        end
+
+
     end
     
-    if(ipos_operacao(aux) > IopMin && ipos_operacao(aux) > k*ipos_resistencia(aux) && deveria_atualizar_pos)
-        instante_do_trip_pos = tempo(aux);
-        deveria_atualizar_pos = 0;
+    if(ipos_operacao(aux) > IopMin && ipos_operacao(aux) > k*ipos_resistencia(aux))
+        sinal_trip_pos(aux) = 1;
+
+        if(aux > tamanho_janela_de_trip && deveria_atualizar_pos)
+            media_pos = mean(sinal_trip_pos(aux - tamanho_janela_de_trip + 1:aux));
+            if(media_pos > 0.5)
+                instante_do_trip_pos = tempo(aux);
+                deveria_atualizar_pos = 0;
+            end
+        end
     end
     
-    if(ineg_operacao(aux) > IopMin && ineg_operacao(aux) > k*ineg_resistencia(aux) && deveria_atualizar_neg)
-        instante_do_trip_neg = tempo(aux);
-        deveria_atualizar_neg = 0;
+    if(ineg_operacao(aux) > IopMin && ineg_operacao(aux) > k*ineg_resistencia(aux))
+        sinal_trip_neg(aux) = 1;
+
+        if(aux > tamanho_janela_de_trip && deveria_atualizar_neg)
+            media_neg = mean(sinal_trip_neg(aux - tamanho_janela_de_trip + 1:aux));
+            if(media_neg > 0.5)
+                instante_do_trip_neg = tempo(aux);
+                deveria_atualizar_neg = 0;
+            end
+        end
     end
     
 
@@ -216,16 +228,170 @@ while aux<length(tempo)
     aux = aux + 1;
 end
 
-% figure(3)
-% hold on;
-% zoom on;
-% grid on;
-% plot(tempo,Ia_1_10_f);
-% plot(tempo,ia_fasores);
+%% ------------------------------------------------------------------------
+% 6) Impressões
+% -------------------------------------------------------------------------
 
-% plot([instante_do_trip, instante_do_trip], ylim, 'r--');
+% 6.1) Valores de fase locais
 
-% title(["Atuação da função de proteção implementada"]);
-% legend('sinal analógico iA_{10}', 'módulo dos fasores IA_{10}', 'instante de percepção da falta','instante do trip');
+neutro_local = Ia_local_f + Ib_local_f + Ic_local_f;
+figure;
+subplot(2, 2, 1);
+plot(tempo, Ia_local_f,'r');
+hold on
+plot(tempo, [ia_local_fasores.magnitude],'k');
+title(["Fase A Local do arquivo " arquivo]);
+ylabel("Ia_{local} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada', 'Módulo do fasor calculado')
+grid;
 
+subplot(2, 2, 2);
+plot(tempo, Ib_local_f,'g');
+hold on
+plot(tempo, [ib_local_fasores.magnitude],'k');
+title(["Fase B Local do arquivo " arquivo]);
+ylabel("Ib_{local} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada', 'Módulo do fasor calculado')
+grid;
+
+subplot(2, 2, 3);
+plot(tempo, Ic_local_f,'b');
+hold on
+plot(tempo, [ic_local_fasores.magnitude],'k');
+title(["Fase C Local do arquivo " arquivo]);
+ylabel("Ic_{local} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada', 'Módulo do fasor calculado')
+grid;
+
+subplot(2, 2, 4);
+plot(tempo, neutro_local);
+title(["Corrente de neutro local do arquivo " arquivo]);
+ylabel("Ineutro_{local} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada')
+grid;
+
+% 6.2) Valores de fase remotos
+
+neutro_remoto = Ia_remoto_f + Ib_remoto_f + Ic_remoto_f;
+figure;
+subplot(2, 2, 1);
+plot(tempo, Ia_remoto_f,'r');
+hold on
+plot(tempo, [ia_remoto_fasores.magnitude],'k');
+title(["Fase A remoto do arquivo " arquivo]);
+ylabel("Ia_{remoto} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada', 'Módulo do fasor calculado')
+grid;
+
+subplot(2, 2, 2);
+plot(tempo, Ib_remoto_f,'g');
+hold on
+plot(tempo, [ib_remoto_fasores.magnitude],'k');
+title(["Fase B remoto do arquivo " arquivo]);
+ylabel("Ib_{remoto} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada', 'Módulo do fasor calculado')
+grid;
+
+subplot(2, 2, 3);
+plot(tempo, Ic_remoto_f,'b');
+hold on
+plot(tempo, [ic_remoto_fasores.magnitude],'k');
+title(["Fase C remoto do arquivo " arquivo]);
+ylabel("Ic_{remoto} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada', 'Módulo do fasor calculado')
+grid;
+
+subplot(2, 2, 4);
+plot(tempo, neutro_remoto);
+title(["Corrente de neutro remoto do arquivo " arquivo]);
+ylabel("Ineutro_{remoto} [A]");
+xlabel("Tempo [s]");
+legend('Corrente filtrada')
+grid;
+
+% 6.3) Componentes simétricas locais
+figure;
+subplot(3, 1, 1);
+plot(tempo, [abs(izero_local)],'r');
+title(["Magnitude da Seq. zero Local do arquivo " arquivo]);
+ylabel("I zero_{local} [A]");
+xlabel("Tempo [s]");
+grid;
+
+subplot(3, 1, 2);
+plot(tempo, [abs(ipos_local)],'g');
+title(["Magnitude da Seq. positiva Local do arquivo " arquivo]);
+ylabel("I positiva_{local} [A]");
+xlabel("Tempo [s]");
+grid;
+
+subplot(3, 1, 3);
+plot(tempo, [abs(ineg_local)],'b');
+title(["Magnitude da Seq. negativa Local do arquivo " arquivo]);
+ylabel("I negativa_{local} [A]");
+xlabel("Tempo [s]");
+grid;
+
+% 6.3) Componentes simétricas remotas
+figure;
+subplot(3, 1, 1);
+plot(tempo, [abs(izero_remoto)],'r');
+title(["Magnitude da Seq. zero remoto do arquivo " arquivo]);
+ylabel("I zero_{remoto} [A]");
+xlabel("Tempo [s]");
+grid;
+
+subplot(3, 1, 2);
+plot(tempo, [abs(ipos_remoto)],'g');
+title(["Magnitude da Seq. positiva remoto do arquivo " arquivo]);
+ylabel("I positiva_{remoto} [A]");
+xlabel("Tempo [s]");
+grid;
+
+subplot(3, 1, 3);
+plot(tempo, [abs(ineg_remoto)],'b');
+title(["Magnitude da Seq. negativa remoto do arquivo " arquivo]);
+ylabel("I negativa_{remoto} [A]");
+xlabel("Tempo [s]");
+grid;
+
+
+% 6.4) Sinais de trip e atuação
+figure;
+subplot(3, 1, 1);
+hold on;
+plot(tempo, sinal_trip_zero,'r');
+plot([instante_do_trip_zero, instante_do_trip_zero], ylim, 'r--');
+title(["Atuação do Relé para Seq. Zero do arquivo " arquivo]);
+ylabel("Nível");
+xlabel("Tempo [s]");
+legend('Nível do sinal de trip', 'Instante de atuação');
+grid;
+
+subplot(3, 1, 2);
+hold on;
+plot(tempo, sinal_trip_pos,'g');
+plot([instante_do_trip_pos, instante_do_trip_pos], ylim, 'r--');
+title(["Atuação do Relé para Seq. Positiva do arquivo " arquivo]);
+ylabel("Nível");
+xlabel("Tempo [s]");
+legend('Nível do sinal de trip', 'Instante de atuação');
+grid;
+
+subplot(3, 1, 3);
+hold on;
+plot(tempo, sinal_trip_neg,'b');
+plot([instante_do_trip_neg, instante_do_trip_neg], ylim, 'r--');
+title(["Atuação do Relé para Seq. Negativa do arquivo " arquivo]);
+ylabel("Nível");
+xlabel("Tempo [s]");
+legend('Nível do sinal de trip', 'Instante de atuação');
+grid;
 
